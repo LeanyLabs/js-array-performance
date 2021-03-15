5b7e7b26e5fb3ed4222c4204cdb3906531c6073a
Andriy Obrizan <andriy@leanylabs.com>
2021-03-15 12:33:40 +0200

    initial commit

---
+++ src/benchmarks.js
@@ -0,0 +1,100 @@
+const Benchmark = require("benchmark");
+
+function buildForEachSuite(array) {
+  return new Benchmark.Suite("forEach")
+    .add("Array.forEach", function () {
+      array.forEach((x) => {
+        x.r = x.a + x.b;
+      });
+    })
+    .add("for of", function () {
+      for (const obj of array) {
+        obj.r = obj.a + obj.b;
+      }
+    })
+    .add("for <array.length, indexing", function () {
+      for (let i = 0; i < array.length; ++i) {
+        array[i].r = array[i].a + array[i].b;
+      }
+    })
+    .add("for <len, indexing", function () {
+      const len = array.length;
+      for (let i = 0; i < len; ++i) {
+        array[i].r = array[i].a + array[i].b;
+      }
+    })
+    .add("for <array.length, tmp element", function () {
+      for (let i = 0; i < array.length; ++i) {
+        const x = array[i];
+        x.r = x.a + x.b;
+      }
+    })
+    .add("for <len, tmp element", function () {
+      const len = array.length;
+      for (let i = 0; i < len; ++i) {
+        const x = array[i];
+        x.r = x.a + x.b;
+      }
+    });
+}
+
+function buildMapSuite(array) {
+  return new Benchmark.Suite("map")
+    .add("Array.map", function () {
+      return array.map((x) => x.a + x.b);
+    })
+    .add("Array.map, destructuring", function () {
+      return array.map(({ a, b }) => a + b);
+    })
+    .add("for of", function () {
+      const result = [];
+      for (const obj of array) {
+        result.push(obj.a + obj.b);
+      }
+      return result;
+    })
+    .add("for of, destructuring", function () {
+      const result = [];
+      for (const { a, b } of array) {
+        result.push(a + b);
+      }
+      return result;
+    })
+    .add("for, init array", function () {
+      const result = new Array(array.length);
+      for (let i = 0; i < array.length; ++i) {
+        result[i] = array[i].a + array[i].b;
+      }
+      return result;
+    });
+}
+
+function buildReduceSuite(array) {
+  return new Benchmark.Suite("reduce")
+    .add("Array.reduce", function () {
+      return array.reduce((p, x) => p + x.a + x.b, 0);
+    })
+    .add("Array.reduce, destructuring", function () {
+      return array.reduce((p, { a, b }) => p + a + b, 0);
+    })
+    .add("for of", function () {
+      let result = 0;
+      for (const { a, b } of array) {
+        result += a + b;
+      }
+      return result;
+    })
+    .add("for", function () {
+      let result = 0;
+      for (let i = 0; i < array.length; ++i) {
+        result += array[i].a + array[i].b;
+      }
+      return result;
+    });
+}
+
+module.exports = {
+  buildReduceSuite,
+  buildForEachSuite,
+  buildMapSuite,
+};
---
+++ src/index.js
@@ -0,0 +1,36 @@
+const {
+  buildForEachSuite,
+  buildMapSuite,
+  buildReduceSuite,
+} = require("./benchmarks");
+
+function runSuite(suite) {
+  console.log(`\nBenchmarking ${suite.name}:`);
+
+  suite
+    .on("cycle", function (event) {
+      console.log(String(event.target));
+    })
+    .on("complete", function () {
+      console.log(this.filter("fastest").map("name") + " is faster");
+    })
+    .run();
+}
+
+function generateTestArray() {
+  const result = [];
+  for (let i = 0; i < 1000000; ++i) {
+    result.push({
+      a: i,
+      b: i / 2,
+      r: 0,
+    });
+  }
+  return result;
+}
+
+const testArray = generateTestArray();
+
+runSuite(buildForEachSuite(testArray));
+runSuite(buildMapSuite(testArray));
+runSuite(buildReduceSuite(testArray));
